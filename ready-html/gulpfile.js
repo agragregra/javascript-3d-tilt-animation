8f546274987a5831f3812de23b9f25b988f7553a
Алексей <agragregra@ya.ru>
2021-08-17 08:17:15 +0300

    Update

--- ready-html/gulpfile.js
+++ ready-html/gulpfile.js
@@ -15,8 +15,7 @@ const stylglob     = require("gulp-empty")
 const cleancss     = require('gulp-clean-css')
 const autoprefixer = require('gulp-autoprefixer')
 const rename       = require('gulp-rename')
-const imagemin     = require('gulp-imagemin')
-const newer        = require('gulp-newer')
+const imagecomp    = require("compress-images")
 const rsync        = require('gulp-rsync')
 const del          = require('del')
 
@@ -70,12 +69,21 @@ function styles() {
 		.pipe(browserSync.stream())
 }
 
-function images() {
-	return src(['app/images/src/**/*'])
-		.pipe(newer('app/images/dist'))
-		.pipe(imagemin())
-		.pipe(dest('app/images/dist'))
-		.pipe(browserSync.stream())
+async function images() {
+	imagecomp(
+		"app/images/src/**/*",
+		"app/images/dist/",
+		{ compress_force: false, statistic: true, autoupdate: true }, false,
+		{ jpg: { engine: "mozjpeg", command: ["-quality", "75"] } },
+		{ png: { engine: "pngquant", command: ["--quality=100", "-o"] } },
+		{ svg: { engine: "svgo", command: "--multipass" } },
+		{ gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+		function (err, completed) {
+			if (completed === true) {
+				browserSync.reload()
+			}
+		}
+	)
 }
 
 function buildcopy() {
@@ -125,5 +133,5 @@ exports.styles  = styles
 exports.images  = images
 exports.deploy  = deploy
 exports.assets  = series(scripts, styles, images)
-exports.build   = series(cleandist, scripts, styles, images, buildcopy, buildhtml)
+exports.build   = series(cleandist, images, scripts, styles, buildcopy, buildhtml)
 exports.default = series(scripts, styles, images, parallel(browsersync, startwatch))
